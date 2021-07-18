e390ed9103dcfb1b5f806a1c693015f853d5b3f6
Jordan K <jordanking0324@gmail.com>
2021-07-18 13:25:10 -0400

    sensible clang-tidy settings, removed utils

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,5 @@
 #include <fmt/format.h>
-#include <fmt/ranges.h>
-
-#include "utils/rect.hpp"
-#include "utils/rng.hpp"
 
 int main() {
-  auto rng = Rng{10172000};
-
-  auto items = std::vector{1, 2, 3, 4, 5, 6, 7};
-  fmt::print("Before: {}\n", items);
-  rng.shuffle(items);
-  fmt::print("After: {}\n", items);
+  fmt::print("Hello World!\n");
 }
